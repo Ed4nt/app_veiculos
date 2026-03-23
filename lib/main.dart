920c940fab336fb81c7e5ff24643ae4906ceab1f
Edelson Bessa <edelsonbessa@gmail.com>
2026-03-23 13:38:17 -0300

    Criar UI para o app de veiculos: Adicionado appBar e mensagem de boas vindas Adicionando TextFields

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple)
+          ),
+      home: MyHomePage(
+        title: 'Cadastro de Veículos'
+        ),
+      
+
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20), 
+          child: Column(
+            children: [
+              Text('Bem-vindo ao app de veículos'),
+              TextField(
+                
+              )
+            ],
+          )
+      ),
+    );
+  }
+
+}
+
+
+
